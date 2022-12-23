43be5dbdb2c8b83316a0c216b3db3309f087f4ed
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2022-12-23 23:11:58 +0600

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Jibon Tai khabar Designed By Team-8 <a href="https://github.com/FaysalMehrab/CSE311_Sec_7_Project">github</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
